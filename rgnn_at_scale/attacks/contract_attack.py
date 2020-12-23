59a88fdef6d8089335de7bbb77815017724e7609
Omar Hedeya <ge82fih@mytum.de>
2020-12-23 22:42:17 +0100

    Tqdm bar for contract now disappears after finish

--- rgnn_at_scale/attacks/contract_attack.py
+++ rgnn_at_scale/attacks/contract_attack.py
@@ -6,6 +6,9 @@ from typing import Union
 from rgnn_at_scale.models import DenseGCN
 from copy import deepcopy
 
+#! For beauty of tqdm bar
+import time
+#!---------------
 #//from .utils import flip_edges_
 
 class EXPAND_CONTRACT():
@@ -17,11 +20,10 @@ class EXPAND_CONTRACT():
         logits = self.model.to(self.device)(self.X, self.adj)
         return -F.cross_entropy(logits[self.idx_attack], self.labels[self.idx_attack])
 
-    def flip_edges(self,adj, row, col, edge):
-        new_edge_value = -adj[ row[edge], col[edge] ] + 1
-        adj[row[edge], col[edge]] = new_edge_value
-        adj[col[edge], row[edge]] = new_edge_value
-        return adj
+    def flip_edges(self, adj, row, col, edge):
+        new_edge_value = -self.adj[ row[edge], col[edge] ] + 1
+        self.adj[row[edge], col[edge]] = new_edge_value
+        self.adj[col[edge], row[edge]] = new_edge_value
 
 
     def contract(self, adj, cohort, row, col, s):
@@ -33,10 +35,10 @@ class EXPAND_CONTRACT():
             for i, edge in enumerate(cohort):
                 #* flip the edge in the adjacency matrix, it is a dense matrix!
                 #//flip_edges_(graph.a, row[edge], col[edge])
-                adj = self.flip_edges(adj, row, col, edge)
+                self.flip_edges(adj, row, col, edge)
                 llhs[i] = self.log_likelihood()                
                 #//flip_edges_(graph.a, row[edge], col[edge])
-                adj = self.flip_edges(adj, row, col, edge)
+                self.flip_edges(adj, row, col, edge)
 
             #? What is the type of llhs? I assume numpy array from the call of np.argpartition
             #? but from .cpu() it means it was a pyTorch Tensor and then we turn it into numpy. 
@@ -49,10 +51,11 @@ class EXPAND_CONTRACT():
             for edge in [cohort[i] for i in np.argpartition(llhs, n_undo)[:n_undo]]:
                 cohort.remove(edge)
                 #//flip_edges_(graph.a, row[edge], col[edge])
-                adj = self.flip_edges(adj, row, col, edge)
+                self.flip_edges(adj, row, col, edge)
                 s[edge] = True
             bar.update(n_undo)
-        return adj
+            #time.sleep(0.5)
+        bar.close()
 
     def __init__(self,   
                  adj: torch.sparse.FloatTensor , 
@@ -105,18 +108,18 @@ class EXPAND_CONTRACT():
                n_perturbations: int, 
                **kwargs):
 
-        alpha = self.alpha
+        #? I think m is just n_perturbations and we do not need it in our code!
         m = self.m
+
+        alpha = self.alpha
         k = self.k
         rng = self.rng
         n = self.n
         step = self.step
         protected = self.protected
-
         adj = self.adj
         X = self.X
         
-
         #Store for each edge if it is eligible for flipping
         s = np.ones(self.n * (self.n - 1) // 2)
 
@@ -145,9 +148,11 @@ class EXPAND_CONTRACT():
         if n_expand > 0:
             for _ in range(n_perturbations - 1): #// in range(m-1)
                 self.contract(adj,cohort,row,col,s)
+
                 ll = self.log_likelihood()
                 bar.set_description(f"Cycles (decr {clean_ll - ll:.5f})")
                 bar.update()
+
                 # Expand the cohort again
                 new_edges = rng.choice(s.size, size=n_expand, replace=False, p=s / s.sum())
                 #//flip_edges_(graph.a, row[new_edges], col[new_edges])
@@ -161,7 +166,7 @@ class EXPAND_CONTRACT():
         #//graph.a = orig_a
         #? Does self.adj or just adj makes a difference here?? 
         #? If self.adj is the same as adj, are we saving the original adj for new attacks or destroying it?
-        self.adj_adversary = adj.to_sparse().detach()
+        self.adj_adversary = self.adj.to_sparse().detach()
         self.attr_adversary = self.X
 
         #? What was this line supposed to do?
