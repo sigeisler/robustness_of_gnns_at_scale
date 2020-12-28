b91fd110a8ece8882aaa7e7c94fea28a5df9ffc6
Omar Hedeya <ge82fih@mytum.de>
2020-12-28 11:26:02 +0100

    Helper Functions of contract attack are private

--- rgnn_at_scale/attacks/contract_attack.py
+++ rgnn_at_scale/attacks/contract_attack.py
@@ -13,20 +13,20 @@ import time
 
 class EXPAND_CONTRACT():
     #! Helper functions
-    def log_likelihood(self):
+    def __log_likelihood(self):
         #? graph is the adjacency mattrix and attributes, while graph.y is labels
         #? model(graph) should produce predictions based on the type of Model GDC, etc...
 
         logits = self.model.to(self.device)(self.X, self.adj)
         return -F.cross_entropy(logits[self.idx_attack], self.labels[self.idx_attack])
 
-    def flip_edges(self, adj, row, col, edge):
+    def __flip_edges(self, adj, row, col, edge):
         new_edge_value = -self.adj[ row[edge], col[edge] ] + 1
         self.adj[row[edge], col[edge]] = new_edge_value
         self.adj[col[edge], row[edge]] = new_edge_value
 
 
-    def contract(self, adj, cohort, row, col, s):
+    def __contract(self, adj, cohort, row, col, s):
         # Contract the cohort until we are in budget again
         bar = tqdm(total=len(cohort) - self.k, leave=False, desc="Contract")
         while len(cohort) > self.k:
@@ -35,10 +35,10 @@ class EXPAND_CONTRACT():
             for i, edge in enumerate(cohort):
                 #* flip the edge in the adjacency matrix, it is a dense matrix!
                 #//flip_edges_(graph.a, row[edge], col[edge])
-                self.flip_edges(adj, row, col, edge)
-                llhs[i] = self.log_likelihood()                
+                self.__flip_edges(adj, row, col, edge)
+                llhs[i] = self.__log_likelihood()                
                 #//flip_edges_(graph.a, row[edge], col[edge])
-                self.flip_edges(adj, row, col, edge)
+                self.__flip_edges(adj, row, col, edge)
 
             #? What is the type of llhs? I assume numpy array from the call of np.argpartition
             #? but from .cpu() it means it was a pyTorch Tensor and then we turn it into numpy. 
@@ -51,7 +51,7 @@ class EXPAND_CONTRACT():
             for edge in [cohort[i] for i in np.argpartition(llhs, n_undo)[:n_undo]]:
                 cohort.remove(edge)
                 #//flip_edges_(graph.a, row[edge], col[edge])
-                self.flip_edges(adj, row, col, edge)
+                self.__flip_edges(adj, row, col, edge)
                 s[edge] = True
             bar.update(n_undo)
             #time.sleep(0.5)
@@ -135,32 +135,32 @@ class EXPAND_CONTRACT():
         n_edges = np.count_nonzero(s)
         assert n_edges >= k
 
-        clean_ll = self.log_likelihood()
+        clean_ll = self.__log_likelihood()
         bar = tqdm(total=n_perturbations, desc="Cycles") #//total = m
         # Randomly select a cohort of edges to flip
         cohort_size = min(int(self.alpha * self.k), n_edges)
         cohort = list(self.rng.choice(s.size, size=cohort_size, replace=False, p=s / s.sum()))
         #//flip_edges_(graph.a, row[cohort], col[cohort])
-        self.flip_edges(adj, row, col, cohort)
+        self.__flip_edges(adj, row, col, cohort)
         s[cohort] = False
 
         n_expand = min(round((self.alpha - 1.0) * k), n_edges - k)
         if n_expand > 0:
             for _ in range(n_perturbations - 1): #// in range(m-1)
-                self.contract(adj,cohort,row,col,s)
+                self.__contract(adj,cohort,row,col,s)
 
-                ll = self.log_likelihood()
+                ll = self.__log_likelihood()
                 bar.set_description(f"Cycles (decr {clean_ll - ll:.5f})")
                 bar.update()
 
                 # Expand the cohort again
                 new_edges = rng.choice(s.size, size=n_expand, replace=False, p=s / s.sum())
                 #//flip_edges_(graph.a, row[new_edges], col[new_edges])
-                self.flip_edges(adj, row, col, new_edges)
+                self.__flip_edges(adj, row, col, new_edges)
                 s[new_edges] = False
                 cohort.extend(list(new_edges))
 
-        self.contract(adj,cohort,row,col, s)
+        self.__contract(adj,cohort,row,col, s)
         bar.update()
 
         #//graph.a = orig_a
