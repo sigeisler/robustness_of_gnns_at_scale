01b5c5e276bb43e06f981f0fd0570f68ea5d52ab
Omar Hedeya <ge82fih@mytum.de>
2021-01-04 23:30:16 +0100

    remove useless lines of code from dice

--- rgnn_at_scale/attacks/dice.py
+++ rgnn_at_scale/attacks/dice.py
@@ -66,7 +66,7 @@ class DICE(object):
         # n is the number of nodes        
         self.n = adj.size()[0]
         self.adj = adj
-        #TODO: Adding dictionary, checking if matrix is symmetrical
+        #TODO: Transform matrix to symmetrical using utils.to_symmetrical
         self.adj_dict = self._to_dict_symmetric(adj)
         self.is_symmetric = True
         # ? why cpu?? apparently some operations can not be performed on GPU(what are they and are we using them??)
@@ -80,7 +80,7 @@ class DICE(object):
         self.add_ratio = add_ratio
 
     #!Private Helper Functions
-    def __deletingEdges(self, delete_budget, adj, labels):
+    def _deletingEdges(self, delete_budget, labels):
         pbar = tqdm(total=delete_budget, desc='removing edges...')
         to_be_deleted_set = set()
         adj_dict =self.adj_dict
@@ -103,18 +103,15 @@ class DICE(object):
                         to_be_deleted_set.add((second_node, first_node))
         pbar.close()
         return to_be_deleted_set
-    
-    def __addingEdges(self, labels, adj, add_budget):
+    def _addingEdges(self, labels, add_budget):
         # add edges till we fill the budget
         pbar = tqdm(total=add_budget, desc='adding edges...')
-        to_be_added_set = set()
         while add_budget > 0:
             source = np.random.randint(self.n)
             dest = np.random.randint(self.n)
             source, dest = (source, dest) if source < dest else (dest, source)
             connected_nodes = self.adj_dict.get(source, [])
-            # We only connect two nodes if they do not have the same classification and 
-            # are not already connected
+            # We only connect two nodes if they do not have the same classification and are not already connected
             if (
                 source != dest
                 and labels[source] != labels[dest]
@@ -124,10 +121,8 @@ class DICE(object):
                 self.adj_dict[source] = connected_nodes
                 add_budget -= 1
                 pbar.update(1)
-        pbar.close()
-        return set()
-    
-    def __performAttack(self, to_be_deleted_set, to_be_added_set, adj):
+        pbar.close()    
+    def _performAttack(self, to_be_deleted_set):
         for pair in to_be_deleted_set:
             first_node = pair[0]
             second_node = pair[1]
@@ -135,22 +130,7 @@ class DICE(object):
             #If node become orphan remove it
             if not self.adj_dict[first_node]:
                 self.adj_dict.pop(first_node, None)
-
-    def attack(self,
-               n_perturbations: int,
-               attack_seed: int = 0,
-               **kwargs):
-        np.random.seed(attack_seed)
-        adj = self.adj
-        labels = self.labels
-        add_budget = int(n_perturbations * self.add_ratio)
-        delete_budget = n_perturbations - add_budget
-        # ? The way this is handled is weird. If there is any self connection, all diagonals get set to zero, then after attack is done, all nodes are set to be having self loops, why?
-        # Prepare edges(connections) to be deleted and others to be added
-        to_be_deleted_set = self.__deletingEdges( delete_budget, adj, labels)
-        to_be_added_set = self.__addingEdges(labels, adj, add_budget)
-        # Perform the attack and update the self.adj_adversary Matrix
-        self.__performAttack(to_be_deleted_set, to_be_added_set, adj)
+    def _from_dict_to_sparse(self):
         indices = []
         values = []
         for key, connected_nodes in self.adj_dict.items():
@@ -162,5 +142,23 @@ class DICE(object):
                 values.append(1)
         i = torch.LongTensor(indices)
         v = torch.FloatTensor(values)
-        self.adj_adversary = torch.sparse.FloatTensor(i.t(), v, torch.Size([self.n, self.n]))
+        return torch.sparse.FloatTensor(i.t(), v, torch.Size([self.n, self.n]))
+    
+    def attack(self,
+               n_perturbations: int,
+               attack_seed: int = 0,
+               **kwargs):
+        np.random.seed(attack_seed)
+        labels = self.labels
+        add_budget = int(n_perturbations * self.add_ratio)
+        delete_budget = n_perturbations - add_budget
+        # ? The way this is handled is weird. If there is any self connection, all diagonals get set to zero, then after attack is done, all nodes are set to be having self loops, why?
+
+        # Prepare edges(connections) to be deleted
+        to_be_deleted_set = self._deletingEdges( delete_budget, labels)
+        self._addingEdges(labels, add_budget)
+        # Perform the delete
+        self._performAttack(to_be_deleted_set)
+        #change dictionary back to sparse matrix
+        self.adj_adversary = self._from_dict_to_sparse()
         self.adj = self.adj_adversary
