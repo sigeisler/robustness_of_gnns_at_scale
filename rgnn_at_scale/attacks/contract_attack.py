54c2f6a388d5b0f10f06387c66e066d52f24b9bd
Omar Hedeya <ge82fih@mytum.de>
2021-01-12 11:08:03 +0100

    Much faster implementation using extra tensors

--- rgnn_at_scale/attacks/contract_attack.py
+++ rgnn_at_scale/attacks/contract_attack.py
@@ -35,9 +35,7 @@ class EXPAND_CONTRACT():
         self.n = adj.size()[0]
         self.adj = adj
         adj_symmetric_index, adj_symmetric_weights = utils.to_symmetric(adj.indices(), adj.values(), self.n)
-        self.adj_dict = self._to_dict_symmetric(adj_symmetric_index, adj_symmetric_weights)
-        #?self.adj_symmetric_index = adj_symmetric_index
-        #?self.adj_symmetric_weights = adj_symmetric_weights
+        self.adj_dict, self.adj_symmetric_index, self.adj_symmetric_weights = self._to_dict_symmetric(adj_symmetric_index, adj_symmetric_weights)
         self.X = X
         self.protected = protected
         self.step = step
@@ -65,40 +63,51 @@ class EXPAND_CONTRACT():
         v = torch.FloatTensor(values)
         return torch.sparse.FloatTensor(i.t(), v, torch.Size([self.n, self.n]))
 
-    def _log_likelihood(self, adj_dict):
-        #Todo Extremely Inefficient, how can it be done better?
-        adj = self._from_dict_to_sparse(adj_dict)
+    def _log_likelihood(self, adj_dict, adj_symmetric_index, adj_symmetric_weights):
+        #adj = self._from_dict_to_sparse(adj_dict)
+        adj = torch.sparse.FloatTensor(adj_symmetric_index, adj_symmetric_weights, torch.Size([self.n, self.n]))
         logits = self.model.to(self.device)(self.X, adj)
         return -F.cross_entropy(logits[self.idx_attack], self.labels[self.idx_attack])
 
-    def _flip_edges(self, adj_dict, edges):
+    def _delete_edge_from_tensor(self, source, dest, adj_symmetric_index, adj_symmetric_weights):
+        indices = torch.nonzero( (adj_symmetric_index[0] == source), as_tuple=True  )[0]
+        dest_index = indices[ torch.nonzero( (adj_symmetric_index[1][indices] == dest), as_tuple=True  )[0] ]
+        adj_symmetric_index = torch.cat( (   adj_symmetric_index[:, 0:dest_index  ],  adj_symmetric_index[:, dest_index+1:]   ), 1 )
+        adj_symmetric_weights = torch.cat( (   adj_symmetric_weights[0:dest_index],  adj_symmetric_weights[dest_index+1:]   ) )
+        return adj_symmetric_index, adj_symmetric_weights
+
+    def _flip_edges(self, adj_dict, edges, adj_symmetric_index, adj_symmetric_weights):
         if type(edges) is not list:
             edges = [edges]
         for source, dest in edges:
             if( not adj_dict.get((source, dest)) ):
                 adj_dict[(source, dest)] = 1
                 #?keep lists updated to make log_likelihood function more computationally efficient??
-                #?self.adj_symmetric_index[0].append(source)
-                #?self.adj_symmetric_index[1].append(dest)
+                adj_symmetric_index = torch.cat((adj_symmetric_index, torch.LongTensor([[source], [dest] ])), 1)
+                adj_symmetric_weights = torch.cat((adj_symmetric_weights, torch.tensor([1])))
+
             else:
                 adj_dict.pop((source, dest), None)
+                adj_symmetric_index, adj_symmetric_weights = self._delete_edge_from_tensor(source, dest, adj_symmetric_index, adj_symmetric_weights)
+        return adj_symmetric_index, adj_symmetric_weights
 
-    def _contract(self, adj_dict, cohort):
+    def _contract(self, adj_dict, cohort, adj_symmetric_index, adj_symmetric_weights):
         # Contract the cohort until we are in budget again
         bar = tqdm(total=len(cohort) - self.k, leave=False, desc="Contract")
         while len(cohort) > self.k:
             llhs =  np.empty( len(cohort) )
             for i, edge in enumerate(cohort):
-                self._flip_edges(adj_dict, edge)
-                llhs[i] = self._log_likelihood(adj_dict)                
-                self._flip_edges(adj_dict, edge)
+                adj_symmetric_index, adj_symmetric_weights = self._flip_edges(adj_dict, edge, adj_symmetric_index, adj_symmetric_weights)
+                llhs[i] = self._log_likelihood(adj_dict, adj_symmetric_index, adj_symmetric_weights)                
+                adj_symmetric_index, adj_symmetric_weights = self._flip_edges(adj_dict, edge, adj_symmetric_index, adj_symmetric_weights)
             # Undo the flips that increase the log-likelihood the least when undone
             n_undo = min(self.step, len(cohort) - self.k)
             for edge in [cohort[i] for i in np.argpartition(llhs, n_undo)[:n_undo]]:
                 cohort.remove(edge)
-                self._flip_edges(adj_dict, edge)
+                adj_symmetric_index, adj_symmetric_weights = self._flip_edges(adj_dict, edge, adj_symmetric_index, adj_symmetric_weights)
             bar.update(n_undo)
         bar.close()
+        return adj_symmetric_index, adj_symmetric_weights
  
     def _add_edges_to_list(self, my_list, number_of_nodes_to_add):
         count = 0
@@ -109,6 +118,7 @@ class EXPAND_CONTRACT():
             if(source != dest):
                 my_list.append((source, dest))
                 count+= 1
+        return my_list
 
     def _extract_upper_triangle_nodes(self, adj_symmetric_index):
         return (adj_symmetric_index[1] > adj_symmetric_index[0])
@@ -133,38 +143,38 @@ class EXPAND_CONTRACT():
         adj_symmetric_index = adj_symmetric_index.to("cpu")
         adj_symmetric_weights = adj_symmetric_weights.to("cpu")
         myAdj = { (source.item(), dest.item()) : weight.item() for (source, dest), weight in zip(adj_symmetric_index.T, adj_symmetric_weights) }
-        return myAdj
+        return myAdj, adj_symmetric_index, adj_symmetric_weights
     
     def attack(self, 
                n_perturbations: int, 
                **kwargs):
         k = self.k
         adj_dict = self.adj_dict.copy()
-        X = self.X
+        adj_symmetric_index = self.adj_symmetric_index.detach().clone()
+        adj_symmetric_weights = self.adj_symmetric_weights.detach().clone()
         #How many possible edges in the upper triangular
         n_edges = (self.n * (self.n - 1)) //2
         assert n_edges >= k
-        clean_ll = self._log_likelihood(adj_dict)
+        clean_ll = self._log_likelihood(adj_dict, adj_symmetric_index, adj_symmetric_weights)
         bar = tqdm(total=n_perturbations, desc="Cycles") 
         # Randomly select a cohort of edges to flip
-        #? Should not we rather choose the indices from len(row)? why do we even need s and counting n_edges this way -> For self.protected in case there is a protected edge!!
         cohort_size = min(int(self.alpha * self.k), n_edges)
         cohort = []
-        self._add_edges_to_list(cohort, cohort_size)
-        self._flip_edges(adj_dict, cohort)
+        cohort = self._add_edges_to_list(cohort, cohort_size)
+        adj_symmetric_index, adj_symmetric_weights = self._flip_edges(adj_dict, cohort, adj_symmetric_index, adj_symmetric_weights)
         n_expand = min(round((self.alpha - 1.0) * k), n_edges - k)
         if n_expand > 0:
             for _ in range(n_perturbations - 1): 
-                self._contract(adj_dict,cohort)
-                ll = self._log_likelihood(adj_dict)
+                adj_symmetric_index, adj_symmetric_weights = self._contract(adj_dict,cohort, adj_symmetric_index, adj_symmetric_weights)
+                ll = self._log_likelihood(adj_dict, adj_symmetric_index, adj_symmetric_weights)
                 bar.set_description(f"Cycles (decr {clean_ll - ll:.5f})")
                 bar.update()
                 new_edges = []
-                self._add_edges_to_list(new_edges, n_expand)
-                self._flip_edges(adj_dict, new_edges)
+                new_edges = self._add_edges_to_list(new_edges, n_expand)
+                adj_symmetric_index, adj_symmetric_weights = self._flip_edges(adj_dict, new_edges, adj_symmetric_index, adj_symmetric_weights)
                 cohort.extend(list(new_edges))
 
-        self._contract(adj_dict,cohort)
+        self._contract(adj_dict,cohort, adj_symmetric_index, adj_symmetric_weights)
         bar.update()
         self.adj_adversary = self._from_dict_to_sparse(adj_dict)
         self.attr_adversary = self.X
