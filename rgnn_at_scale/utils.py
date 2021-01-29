fix: WIP ppr attack update normalization

rgnn_at_scale/utils.py
@@ -121,9 +121,12 @@ def calc_ppr_update(ppr: SparseTensor,
     # sparse normalization: row_diff = row / row.sum()
     row_weights /= row_weights.sum()
 
-    # sparse subtraction: v - A[i]
+    # A_norm = A[i] / A[i].sum()
+    Ai_norm_val = Ai_vals / Ai_vals.sum()
+
+    # sparse subtraction: row_diff = row - A_norm
     row_idx = torch.cat((row_idx, Ai_idx), dim=-1)
-    row_weights = torch.cat((row_weights, Ai_vals * - 1))
+    row_weights = torch.cat((row_weights, Ai_norm_val * - 1))
     row_idx, row_weights = torch_sparse.coalesce(
         row_idx,
         row_weights,
@@ -204,8 +207,9 @@ def calc_ppr_update_dense(ppr: torch.Tensor,
     v = torch.where(A[i] > 0, -p, p)
 
     row = A[i] + v
-    row_norm = row / row.sum()
-    row_diff = row_norm - A[i]
+    row = row / row.sum()
+    A_norm = A[i] / A[i].sum()
+    row_diff = row - A_norm
     row_diff_norm = (alpha - 1) * row_diff
 
     # Sherman Morrison Formular for (P + uv)^-1

tests/ppr_test.py
@@ -22,10 +22,10 @@ class TestPPRUpdate():
         i = 2
         A_dense = torch.tensor([[0, 1, 0, 1],
                                 [1, 0, 1, 0],
-                                [0, 1, 0, 1],
+                                [1, 1, 0, 1],
                                 [1, 1, 1, 0]],
                                dtype=torch.float32)
-        # p = torch.tensor([[0.5, 0.0, 0, 0.3]],
+        # p = torch.tensor([[0, 0.0, 0, 0.3]],
         #                  dtype=torch.float32,
         #                  requires_grad=True)
         # A_dense = torch.tensor([[0, 1, 0, 1],
@@ -180,7 +180,7 @@ class TestPPRUpdate():
         A_pert = A_dense + u@v
         ppr_pert_exact = calc_ppr_exact_row(A_pert, alpha=alpha)
 
-        assert torch.allclose(ppr_pert_update, ppr_pert_exact, atol=1e-05)
+        assert torch.allclose(ppr_pert_update.to_dense(), ppr_pert_exact, atol=1e-05)
 
         ppr_pert_update.sum().backward()
         assert p.grad is not None