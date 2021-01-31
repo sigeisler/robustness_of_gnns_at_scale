aa5f8b58dfb54d445394f388eb968d74a4de9925
Simon Geisler <geisler@in.tum.de>
2021-01-31 23:48:35 +0100

    feat: also support gcn in local prbcd

--- rgnn_at_scale/attacks/local_prbcd.py
+++ rgnn_at_scale/attacks/local_prbcd.py
@@ -1,9 +1,8 @@
 from collections import defaultdict
 from copy import deepcopy
 import math
-from typing import Dict, Optional, Tuple, Union
+from typing import Dict, Optional, Union
 import warnings
-from numpy.core.numeric import identity
 
 from tqdm import tqdm
 from torch.nn import functional as F
@@ -12,10 +11,10 @@ import torch
 import torch_sparse
 from torch_sparse import SparseTensor
 
-from rgnn_at_scale.models import RobustPPRGoWrapper, PPRGoWrapper
+from rgnn_at_scale.models import MODEL_TYPE, BATCHED_PPR_MODELS
 from rgnn_at_scale.utils import calc_ppr_exact_row, calc_ppr_update_sparse_result, grad_with_checkpoint
 from pprgo import ppr
-from .prbcd import PRBCD
+from rgnn_at_scale.attacks.prbcd import PRBCD
 
 
 class LocalPRBCD(PRBCD):
@@ -25,7 +24,7 @@ class LocalPRBCD(PRBCD):
                  X: torch.Tensor,
                  labels: torch.Tensor,
                  idx_attack: np.ndarray,
-                 model: Union[RobustPPRGoWrapper, PPRGoWrapper],
+                 model: MODEL_TYPE,
                  device: Union[str, int, torch.device],
                  ppr_matrix: Optional[SparseTensor] = None,
                  loss_type: str = 'Margin',  # 'CW', 'LeakyCW'  # 'CE', 'MCE', 'Margin'
@@ -34,7 +33,7 @@ class LocalPRBCD(PRBCD):
                  display_step: int = 20,
                  epochs: int = 400,
                  fine_tune_epochs: int = 100,
-                 search_space_size: int = 100,
+                 search_space_size: int = 10_000,
                  with_early_stropping: bool = True,
                  do_synchronize: bool = False,
                  eps: float = 1e-14,
@@ -51,10 +50,10 @@ class LocalPRBCD(PRBCD):
 
         # TODO: Replace adj or remove
         edge_index_rows, edge_index_cols, edge_weight = adj.coo()
-        diagonal_mask = edge_index_rows == edge_index_cols
-        edge_index_rows = edge_index_rows[diagonal_mask]
-        edge_index_cols = edge_index_cols[diagonal_mask]
-        edge_weight = edge_weight[diagonal_mask]
+        #diagonal_mask = edge_index_rows == edge_index_cols
+        #edge_index_rows = edge_index_rows[diagonal_mask]
+        #edge_index_cols = edge_index_cols[diagonal_mask]
+        #edge_weight = edge_weight[diagonal_mask]
         self.adj = SparseTensor(row=edge_index_rows, col=edge_index_cols, value=edge_weight, sparse_sizes=adj.sizes())
 
         self.edge_index = torch.stack([edge_index_rows, edge_index_cols], dim=0).cpu()
@@ -64,14 +63,15 @@ class LocalPRBCD(PRBCD):
         self.d = X.shape[1]
         self.labels = labels.to(self.device)
         self.idx_attack = idx_attack
-        self.ppr_alpha = model.alpha
-        if ppr_matrix is None:
-            self.ppr_matrix = SparseTensor.from_scipy(
-                ppr.topk_ppr_matrix(adj.to_scipy(layout="csr"), model.alpha, model.eps,
-                                    np.arange(self.n), model.topk,  normalization=model.ppr_normalization)
-            ).to(device)
-        else:
-            self.ppr_matrix = ppr_matrix
+        if type(model) in BATCHED_PPR_MODELS.__args__:
+            self.ppr_alpha = model.alpha
+            if ppr_matrix is None:
+                self.ppr_matrix = SparseTensor.from_scipy(
+                    ppr.topk_ppr_matrix(adj.to_scipy(layout="csr"), model.alpha, model.eps,
+                                        np.arange(self.n), model.topk,  normalization=model.ppr_normalization)
+                ).to(device)
+            else:
+                self.ppr_matrix = ppr_matrix
 
         self.loss_type = loss_type
         self.lr_n_perturbations_factor = lr_n_perturbations_factor
@@ -101,23 +101,27 @@ class LocalPRBCD(PRBCD):
 
         for epoch in tqdm(range(self.epochs + self.fine_tune_epochs)):
             self.modified_edge_weight_diff.requires_grad = True
-            updated_ppr_vector = self.get_updated_ppr_vector(node_idx)
+            updated_vector_or_graph = self.get_updated_vector_or_graph(node_idx)
 
             if self.do_synchronize:
                 torch.cuda.empty_cache()
                 torch.cuda.synchronize()
 
-            logits = F.log_softmax(self.model.forward(self.X, None, ppr_scores=updated_ppr_vector), dim=-1)
+            logits = self.get_logits(updated_vector_or_graph)
             loss = self.calculate_loss(logits, self.labels[node_idx][None])
 
             classification_statistics = LocalPRBCD.classification_statistics(logits, self.labels[node_idx])
             if epoch == 0:
                 print(f'Initial: Loss: {loss.item()} Statstics: {classification_statistics} %\n')
                 with torch.no_grad():
-                    ppr_scores_orig = self.ppr_matrix[node_idx]
-                    logits_orig = F.log_softmax(self.model.forward(self.X, None, ppr_scores=ppr_scores_orig), dim=-1)
+                    if type(self.model) in BATCHED_PPR_MODELS.__args__:
+                        updated_vector_or_graph_orig = self.ppr_matrix[node_idx]
+                    else:
+                        updated_vector_or_graph_orig = self.adj.to(self.device)
+                    logits_orig = self.get_logits(updated_vector_or_graph_orig)
                     loss_orig = self.calculate_loss(logits_orig, self.labels[node_idx][None])
-                    statistics_orig = LocalPRBCD.classification_statistics(logits_orig, self.labels[node_idx])
+                    statistics_orig = LocalPRBCD.classification_statistics(logits_orig,
+                                                                           self.labels[node_idx])
                     print(f'Original: Loss: {loss_orig.item()} Statstics: {statistics_orig} %\n')
 
             gradient = grad_with_checkpoint(loss, self.modified_edge_weight_diff)[0]
@@ -134,8 +138,8 @@ class LocalPRBCD(PRBCD):
                     n_perturbations, self.modified_edge_weight_diff, self.eps
                 )
 
-                updated_ppr_vector = self.get_updated_ppr_vector(node_idx)
-                logits = F.log_softmax(self.model.forward(self.X, None, ppr_scores=updated_ppr_vector), dim=-1)
+                updated_vector_or_graph = self.get_updated_vector_or_graph(node_idx)
+                logits = self.get_logits(updated_vector_or_graph)
                 classification_statistics = LocalPRBCD.classification_statistics(logits, self.labels[node_idx])
                 if epoch % self.display_step == 0:
                     print(f'\nEpoch: {epoch} Loss: {loss.item()} Statstics: {classification_statistics} %\n')
@@ -148,53 +152,82 @@ class LocalPRBCD(PRBCD):
             del loss
             del gradient
 
-        self.updated_ppr_vector = self.sample_edges(node_idx, n_perturbations)
+        self.updated_vector_or_graph = self.sample_edges(node_idx, n_perturbations)
 
-        logits = F.log_softmax(self.model.forward(self.X, None, ppr_scores=self.updated_ppr_vector), dim=-1)
+        logits = self.get_logits(updated_vector_or_graph)
         return logits
 
-    def get_updated_ppr_vector(self, node_idx: int) -> SparseTensor:
+    def get_logits(self, updated_vector_or_graph: SparseTensor) -> torch.Tensor:
+        if type(self.model) in BATCHED_PPR_MODELS.__args__:
+            return F.log_softmax(self.model.forward(self.X, None, ppr_scores=updated_vector_or_graph), dim=-1)
+        else:
+            return self.model(data=self.X.to(self.device), adj=updated_vector_or_graph)
+
+    def get_updated_vector_or_graph(self, node_idx: int) -> SparseTensor:
         modified_edge_weight_diff = SparseTensor(row=torch.zeros_like(self.current_search_space),
                                                  col=self.current_search_space,
                                                  value=self.modified_edge_weight_diff,
                                                  sparse_sizes=(1, self.n))
+        if type(self.model) in BATCHED_PPR_MODELS.__args__:
+            A_row = self.adj[node_idx].to(self.device)
+            updated_vector_or_graph = calc_ppr_update_sparse_result(self.ppr_matrix, A_row,
+                                                                    modified_edge_weight_diff, node_idx, self.ppr_alpha)
+            # updated_vector_or_graph /= updated_vector_or_graph.sum()
 
-        A_row = self.adj[node_idx].to(self.device)
-        updated_ppr_vector = calc_ppr_update_sparse_result(self.ppr_matrix, A_row,
-                                                           modified_edge_weight_diff, node_idx, self.ppr_alpha)
-        # updated_ppr_vector /= updated_ppr_vector.sum()
+            # modified_adj = SparseTensor(
+            #     row=torch.full_like(self.current_search_space, node_idx),
+            #     col=self.current_search_space,
+            #     value=self.modified_edge_weight_diff,
+            #     sparse_sizes=(self.n, self.n)
+            # ).to_scipy(layout="csr") + self.adj.to_scipy(layout="csr")
 
-        # modified_adj = SparseTensor(
-        #     row=torch.full_like(self.current_search_space, node_idx),
-        #     col=self.current_search_space,
-        #     value=self.modified_edge_weight_diff,
-        #     sparse_sizes=(self.n, self.n)
-        # ).to_scipy(layout="csr") + self.adj.to_scipy(layout="csr")
+            u = torch.zeros((self.n, 1), dtype=torch.float32, device=self.device)
+            u[node_idx] = 1
+            p_dense = modified_edge_weight_diff.to_dense()
+            A_dense = self.adj.to(self.device).to_dense()
+            v = torch.where(A_dense[node_idx, :] > 0, -p_dense, p_dense)
+            A_pert = A_dense + u @ v
+            exact_ppr_vector = calc_ppr_exact_row(A_pert, alpha=self.ppr_alpha)[node_idx]
 
-        u = torch.zeros((self.n, 1), dtype=torch.float32, device=self.device)
-        u[node_idx] = 1
-        p_dense = modified_edge_weight_diff.to_dense()
-        A_dense = self.adj.to(self.device).to_dense()
-        v = torch.where(A_dense[node_idx, :] > 0, -p_dense, p_dense)
-        A_pert = A_dense + u @ v
-        exact_ppr_vector = calc_ppr_exact_row(A_pert, alpha=self.ppr_alpha)[node_idx]
+            approx_ppr_vector = SparseTensor.from_scipy(
+                ppr.topk_ppr_matrix(SparseTensor.from_dense(A_pert).to_scipy(layout="csr"), self.model.alpha,
+                                    self.model.eps, np.arange(self.n), self.model.topk,
+                                    normalization=self.model.ppr_normalization)
+            ).to(self.device)[node_idx]
+            # approx_ppr_vector.storage._value /= approx_ppr_vector.sum()
 
-        approx_ppr_vector = SparseTensor.from_scipy(
-            ppr.topk_ppr_matrix(SparseTensor.from_dense(A_pert).to_scipy(layout="csr"), self.model.alpha,
-                                self.model.eps, np.arange(self.n), self.model.topk,
-                                normalization=self.model.ppr_normalization)
-        ).to(self.device)[node_idx]
-        # approx_ppr_vector.storage._value /= approx_ppr_vector.sum()
+            diff_approx_exact = torch.norm(approx_ppr_vector.to_dense() - exact_ppr_vector)
+            diff_approx_updated = torch.norm(approx_ppr_vector.to_dense() - updated_vector_or_graph)
+            diff_exact_updated = torch.norm(exact_ppr_vector - updated_vector_or_graph)
 
-        diff_approx_exact = torch.norm(approx_ppr_vector.to_dense() - exact_ppr_vector)
-        diff_approx_updated = torch.norm(approx_ppr_vector.to_dense() - updated_ppr_vector)
-        diff_exact_updated = torch.norm(exact_ppr_vector - updated_ppr_vector)
+            if diff_exact_updated > diff_approx_exact or diff_exact_updated > diff_approx_updated:
+                warnings.warn(f'Error os large diff_approx_exact={diff_approx_exact:.3f} '
+                              f'diff_approx_updated={diff_approx_updated:.3f} '
+                              f'diff_exact_updated={diff_exact_updated:.3f}')
 
-        if diff_exact_updated > diff_approx_exact or diff_exact_updated > diff_approx_updated:
-            warnings.warn(f'Error os large diff_approx_exact={diff_approx_exact:.3f} '
-                          f'diff_approx_updated={diff_approx_updated:.3f} diff_exact_updated={diff_exact_updated:.3f}')
+            return SparseTensor.from_dense(updated_vector_or_graph)
+        else:
+            v_rows, v_cols, v_vals = modified_edge_weight_diff.coo()
+            v_idx = torch.stack([v_rows, v_cols], dim=0)
 
-        return SparseTensor.from_dense(updated_ppr_vector)
+            A_rows, A_cols, A_vals = self.adj.to(self.device).coo()
+            A_idx = torch.stack([A_rows, A_cols], dim=0)
+
+            # sparse addition: row = A[i] + v
+            A_idx = torch.cat((v_idx, A_idx), dim=-1)
+            A_weights = torch.cat((v_vals, A_vals))
+            A_idx, A_weights = torch_sparse.coalesce(
+                A_idx,
+                A_weights,
+                m=1,
+                n=self.n,
+                op='sum'
+            )
+
+            # Works since the attack will always assign at least a small constant the elements in p
+            A_weights[A_weights > 1] = -A_weights[A_weights > 1] + 2
+
+            return SparseTensor.from_edge_index(A_idx, A_weights, (self.n, self.n))
 
     def update_edge_weights(self, n_perturbations: int, epoch: int, gradient: torch.Tensor):
         lr_factor = max(1., n_perturbations / self.n / 2 / self.lr_n_perturbations_factor) * self.lr_factor
@@ -283,8 +316,8 @@ class LocalPRBCD(PRBCD):
                     self.current_search_space = current_search_space[self.modified_edge_weight_diff == 1]
                     self.modified_edge_weight_diff = self.modified_edge_weight_diff[self.modified_edge_weight_diff == 1]
 
-                    updated_ppr_vector = self.get_updated_ppr_vector(node_idx)
-                    logits = F.log_softmax(self.model.forward(self.X, None, ppr_scores=updated_ppr_vector), dim=-1)
+                    updated_vector_or_graph = self.get_updated_vector_or_graph(node_idx)
+                    logits = self.get_logits(updated_vector_or_graph)
                     margin = LocalPRBCD.classification_statistics(logits, self.labels[node_idx])['margin']
                     if best_margin < margin:
                         best_margin = margin
@@ -293,5 +326,5 @@ class LocalPRBCD(PRBCD):
             self.modified_edge_weight_diff = best_weights.to(self.device).float()
             self.current_search_space = best_search_space.to(self.device).long()
 
-            updated_ppr_vector = self.get_updated_ppr_vector(node_idx)
-        return updated_ppr_vector
+            updated_vector_or_graph = self.get_updated_vector_or_graph(node_idx)
+        return updated_vector_or_graph
