211859629db533203b93d023e701aaa1aa423b49
Tobias Schmidt <tob.schmidt@tum.de>
2021-02-08 13:26:41 +0100

    fix: local attack eval

--- experiment_local_attack_evaluate.py
+++ experiment_local_attack_evaluate.py
@@ -43,7 +43,7 @@ def config():
 
     model_labels = ['Vanilla PPRGo']
     seeds = [0, 1, 5]
-    db_collection = "kdd21_local_attack_citeseer"
+    db_collection_attacks = "kdd21_local_attack_citeseer"
     artifact_dir = 'cache'
     model_storage_type = 'attack_citeseer'
     device = "cpu"
@@ -63,12 +63,12 @@ def flip_edges(adj, pert_edge_rows, pert_edge_cols):
 
 
 @ex.automain
-def run(data_dir: str, dataset: str, db_collection: str, binary_attr: bool, make_undirected: bool,
+def run(data_dir: str, dataset: str, db_collection_attacks: str, binary_attr: bool, make_undirected: bool,
         make_unweighted: bool, seeds: int, artifact_dir: str, model_labels: List[str], model_storage_type: str,
         device: Union[str, int], data_device: Union[str, int]):
 
     logging.info({
-        'dataset': dataset, 'db_collection': db_collection, 'binary_attr': binary_attr, 'model_labels': model_labels,
+        'dataset': dataset, 'db_collection_attacks': db_collection_attacks, 'binary_attr': binary_attr, 'model_labels': model_labels,
         'seeds': seeds, 'artifact_dir': artifact_dir,  'model_storage_type': model_storage_type,
         'device': device, "data_device": data_device
     })
@@ -89,7 +89,7 @@ def run(data_dir: str, dataset: str, db_collection: str, binary_attr: bool, make
 
     storage = Storage(artifact_dir, experiment=ex)
 
-    df_attack_experiments = seml.get_results(db_collection,
+    df_attack_experiments = seml.get_results(db_collection_attacks,
                                              to_data_frame=True,
                                              fields=['batch_id', 'slurm', 'config', 'result'])
 
@@ -131,65 +131,66 @@ def run(data_dir: str, dataset: str, db_collection: str, binary_attr: bool, make
                 for _, df_model_atk_result in df_model_atk_results.iterrows():
                     atk_node_ix = df_model_atk_result["node_id"]
                     perturbed_edges = df_model_atk_result["perturbed_edges"]
-                    eps = df_model_atk_result["epsilon"]
-                    logging.info(
-                        f"Recalculating logits and margin for node {atk_node_ix}, epsilon {eps} and seed {seed}")
-                    pert_rows, pert_cols = np.array(perturbed_edges[0]), np.array(perturbed_edges[1])
-                    if make_undirected:
-                        pert_rows = np.concatenate([pert_rows, pert_cols])
-                        pert_cols = np.concatenate([pert_cols, np.array(perturbed_edges[0])])
+                    if len(perturbed_edges) == 2:  # if unsucessfull, the perturbed_edges might be empty
+                        eps = df_model_atk_result["epsilon"]
+                        logging.info(
+                            f"Recalculating logits and margin for node {atk_node_ix}, epsilon {eps} and seed {seed}")
+                        pert_rows, pert_cols = np.array(perturbed_edges[0]), np.array(perturbed_edges[1])
+                        if make_undirected:
+                            pert_rows = np.concatenate([pert_rows, pert_cols])
+                            pert_cols = np.concatenate([pert_cols, np.array(perturbed_edges[0])])
 
-                    if isinstance(adj, SparseTensor):
-                        adj = adj.to_scipy(layout="csr")
+                        if isinstance(adj, SparseTensor):
+                            adj = adj.to_scipy(layout="csr")
 
-                    # TODO: we could remove this again because we have the initial logits in df_model_atk_result
-                    # but it's also a nice sanity check to make sure everything works and is specified correctly
-                    ppr_topk_atk_node = ppr.topk_ppr_matrix(adj.tocsr(), model.alpha, model.eps, np.array(
-                        [atk_node_ix]), model.topk, normalization=model.ppr_normalization)
-                    ppr_topk_atk_node = SparseTensor.from_scipy(ppr_topk_atk_node)
-                    initial_logits = F.log_softmax(model.forward(
-                        attr, None, ppr_scores=ppr_topk_atk_node), dim=-1).cpu()
-                    initial_classification_stats = LocalPRBCD.classification_statistics(initial_logits,
-                                                                                        labels[atk_node_ix].cpu())
+                        # TODO: we could remove this again because we have the initial logits in df_model_atk_result
+                        # but it's also a nice sanity check to make sure everything works and is specified correctly
+                        ppr_topk_atk_node = ppr.topk_ppr_matrix(adj.tocsr(), model.alpha, model.eps, np.array(
+                            [atk_node_ix]), model.topk, normalization=model.ppr_normalization)
+                        ppr_topk_atk_node = SparseTensor.from_scipy(ppr_topk_atk_node)
+                        initial_logits = F.log_softmax(model.forward(
+                            attr, None, ppr_scores=ppr_topk_atk_node), dim=-1).cpu()
+                        initial_classification_stats = LocalPRBCD.classification_statistics(initial_logits,
+                                                                                            labels[atk_node_ix].cpu())
 
-                    # perturbe adjacency
-                    adj = adj.todok()
-                    flip_edges(adj, pert_rows, pert_cols)
+                        # perturbe adjacency
+                        adj = adj.todok()
+                        flip_edges(adj, pert_rows, pert_cols)
 
-                    ppr_topk_atk_node_pert = ppr.topk_ppr_matrix(adj.tocsr(), model.alpha, model.eps, np.array(
-                        [atk_node_ix]), model.topk, normalization=model.ppr_normalization)
+                        ppr_topk_atk_node_pert = ppr.topk_ppr_matrix(adj.tocsr(), model.alpha, model.eps, np.array(
+                            [atk_node_ix]), model.topk, normalization=model.ppr_normalization)
 
-                    ppr_topk_atk_node_pert = SparseTensor.from_scipy(ppr_topk_atk_node_pert)
-                    pert_logits = F.log_softmax(model.forward(
-                        attr, None, ppr_scores=ppr_topk_atk_node_pert), dim=-1).cpu()
-                    pert_classification_stats = LocalPRBCD.classification_statistics(pert_logits,
-                                                                                     labels[atk_node_ix].cpu())
+                        ppr_topk_atk_node_pert = SparseTensor.from_scipy(ppr_topk_atk_node_pert)
+                        pert_logits = F.log_softmax(model.forward(
+                            attr, None, ppr_scores=ppr_topk_atk_node_pert), dim=-1).cpu()
+                        pert_classification_stats = LocalPRBCD.classification_statistics(pert_logits,
+                                                                                         labels[atk_node_ix].cpu())
 
-                    results.append({
-                        'label': model_label,
-                        'seed': seed,
-                        'attack': df_model_atk_result["attack"],
-                        'dataset': df_model_atk_result["dataset"],
-                        'epsilon': eps,
-                        'n_perturbations': df_model_atk_result["n_perturbations"],
-                        'degree': df_model_atk_result["degree"],
-                        'logits': pert_logits.cpu(),
-                        'initial_logits': initial_logits.cpu(),
-                        'larget': labels[atk_node_ix].item(),
-                        'node_id': atk_node_ix,
-                        'perturbed_edges': perturbed_edges,
-                        'initial_margin_diff': df_model_atk_result["initial_margin"] - initial_classification_stats["margin"],
-                        'margin_diff': df_model_atk_result["margin"] - pert_classification_stats["margin"]
-                    })
-                    results[-1].update(pert_classification_stats)
-                    results[-1].update({
-                        f'initial_{key}': value
-                        for key, value
-                        in initial_classification_stats.items()
-                    })
+                        results.append({
+                            'label': model_label,
+                            'seed': seed,
+                            'attack': df_model_atk_result["attack"],
+                            'dataset': df_model_atk_result["dataset"],
+                            'epsilon': eps,
+                            'n_perturbations': df_model_atk_result["n_perturbations"],
+                            'degree': df_model_atk_result["degree"],
+                            'logits': pert_logits.cpu(),
+                            'initial_logits': initial_logits.cpu(),
+                            'larget': labels[atk_node_ix].item(),
+                            'node_id': atk_node_ix,
+                            'perturbed_edges': perturbed_edges,
+                            'initial_margin_diff': df_model_atk_result["initial_margin"] - initial_classification_stats["margin"],
+                            'margin_diff': df_model_atk_result["margin"] - pert_classification_stats["margin"]
+                        })
+                        results[-1].update(pert_classification_stats)
+                        results[-1].update({
+                            f'initial_{key}': value
+                            for key, value
+                            in initial_classification_stats.items()
+                        })
 
-                    # undo pertubation so we have a clean adjacency for the next iteration
-                    flip_edges(adj, pert_rows, pert_cols)
+                        # undo pertubation so we have a clean adjacency for the next iteration
+                        flip_edges(adj, pert_rows, pert_cols)
 
     assert len(results) > 0
 
