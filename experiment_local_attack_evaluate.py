feat: add dataset normalization for ogbn and other minor changes and fixes

experiment_local_attack_evaluate.py
@@ -156,6 +156,7 @@ def run(data_dir: str, dataset: str, db_collection_attacks: str, binary_attr: bo
                 for _, df_model_atk_result in df_model_atk_results.iterrows():
                     atk_node_ix = df_model_atk_result["node_id"]
                     perturbed_edges = df_model_atk_result["perturbed_edges"]
+                    n_perturbations = df_model_atk_result["n_perturbations"]
                     if len(perturbed_edges) == 2:  # if unsucessfull, the perturbed_edges might be empty
                         eps = df_model_atk_result["epsilon"]
                         logging.info(
@@ -170,8 +171,9 @@ def run(data_dir: str, dataset: str, db_collection_attacks: str, binary_attr: bo
 
                         # TODO: we could remove this again because we have the initial logits in df_model_atk_result
                         # but it's also a nice sanity check to make sure everything works and is specified correctly
+                        topk = model.topk + n_perturbations
                         ppr_topk_atk_node = ppr.topk_ppr_matrix(adj, model.alpha, model.eps, np.array(
-                            [atk_node_ix]), model.topk, normalization=model.ppr_normalization)
+                            [atk_node_ix]), topk, normalization=model.ppr_normalization)
                         ppr_topk_atk_node = SparseTensor.from_scipy(ppr_topk_atk_node)
                         initial_logits = F.log_softmax(model.forward(
                             attr, None, ppr_scores=ppr_topk_atk_node), dim=-1).cpu()
@@ -182,7 +184,7 @@ def run(data_dir: str, dataset: str, db_collection_attacks: str, binary_attr: bo
                         pert_adj = flip_edges(adj, pert_rows, pert_cols)
 
                         ppr_topk_atk_node_pert = ppr.topk_ppr_matrix(pert_adj, model.alpha, model.eps, np.array(
-                            [atk_node_ix]), model.topk, normalization=model.ppr_normalization)
+                            [atk_node_ix]), topk, normalization=model.ppr_normalization)
 
                         ppr_topk_atk_node_pert = SparseTensor.from_scipy(ppr_topk_atk_node_pert)
                         pert_logits = F.log_softmax(model.forward(
@@ -196,7 +198,7 @@ def run(data_dir: str, dataset: str, db_collection_attacks: str, binary_attr: bo
                             'attack': df_model_atk_result["attack"],
                             'dataset': df_model_atk_result["dataset"],
                             'epsilon': eps,
-                            'n_perturbations': df_model_atk_result["n_perturbations"],
+                            'n_perturbations': n_perturbations,
                             'degree': df_model_atk_result["degree"],
                             'logits': pert_logits.cpu(),
                             'initial_logits': initial_logits.cpu(),

experiment_train.py
@@ -51,6 +51,7 @@ def config():
         'max_epochs': 3000
     }
     binary_attr = False
+    normalize = False
     make_undirected = True
     make_unweighted = True
     seed = 0
@@ -61,13 +62,14 @@ def config():
     data_dir = './datasets'
     data_device = 'cpu'
 
+
 @ex.automain
 def run(data_dir: str, dataset: str, model_params: Dict[str, Any], train_params: Dict[str, Any], binary_attr: bool,
-        make_undirected: bool, make_unweighted: bool, seed: int, artifact_dir: str,
+        make_undirected: bool, make_unweighted: bool, normalize: bool, seed: int, artifact_dir: str,
         model_storage_type: str, device: Union[str, int], data_device: Union[str, int], display_steps: int):
     logging.info({
         'dataset': dataset, 'model_params': model_params, 'train_params': train_params, 'binary_attr': binary_attr,
-        'make_undirected': make_undirected, 'make_unweighted': make_unweighted,
+        'make_undirected': make_undirected, 'make_unweighted': make_unweighted, 'normalize': normalize,
         'seed': seed, 'artifact_dir': artifact_dir, 'model_storage_type': model_storage_type, 'device': device,
         'display_steps': display_steps
     })
@@ -76,6 +78,7 @@ def run(data_dir: str, dataset: str, model_params: Dict[str, Any], train_params:
     np.random.seed(seed)
 
     graph = prep_graph(dataset, data_device, dataset_root=data_dir,
+                       normalize=normalize,
                        make_undirected=make_undirected,
                        make_unweighted=make_unweighted,
                        binary_attr=binary_attr,

ppr_load_parallel.py
@@ -20,22 +20,22 @@ from pprgo import ppr
 
 from rgnn_at_scale.local import setup_logging
 from rgnn_at_scale.data import prep_graph, split
+from rgnn_at_scale.load_ppr import load_ppr
 
 setup_logging()
 
-logging.info("start")
 
 # dataset = "ogbn-arxiv"  # "ogbn-papers100M"  # "ogbn-arxiv"
 # dataset_root = "/nfs/students/schmidtt/datasets/"
-# input_dir = dataset_root + "ppr/"
-# topk_batch_size = 10240
+# input_dir = dataset_root + "ppr/arxiv/"
+# topk_batch_size = 10240  # int(1e5)
 # dir_name = '_'.join(dataset.split('-'))
 # num_batches = 17
 # num_nodes = int(169343)
 
-dataset = "ogbn-papers100M"  # "ogbn-arxiv"  # "ogbn-papers100M"  # "ogbn-arxiv"
+dataset = "ogbn-products"  # "ogbn-arxiv"  # "ogbn-papers100M"  # "ogbn-arxiv"
 dataset_root = "/nfs/students/schmidtt/datasets/"
-input_dir = dataset_root + "ppr/papers/"
+input_dir = dataset_root + "ppr/products/"
 binary_attr = False
 topk_batch_size = int(1e6)
 num_nodes = int(111059956)
@@ -43,11 +43,26 @@ num_batches = 112
 
 # ppr params
 alpha = 0.1
-eps = 1e-3
+eps = 1e-4
 topk = 64
+normalize = "row"
 ppr_normalization = "row"
 alpha_suffix = int(alpha * 100)
 
+logging.info(f"Start Simon")
+start_simon = datetime.now()
+ppr_matrix = load_ppr(input_dir=input_dir,
+                      dataset=dataset,
+                      alpha=alpha,
+                      eps=eps,
+                      topk=topk,
+                      ppr_normalization=ppr_normalization)
+simon_time = datetime.now() - start_simon
+logging.info(f"Simon took {simon_time} seconds")
+
+print(ppr_matrix.nnz)
+
+logging.info("start me")
 start = datetime.now()
 
 
@@ -98,54 +113,66 @@ logging.info(inputs)
 #                                                                      ppr_normalization,
 #                                                                      i) for i in inputs)
 
-results = [load_ppr_topk_parallel(topk_batch_size,
-                                  input_dir,
-                                  alpha,
-                                  eps,
-                                  topk,
-                                  ppr_normalization,
-                                  list(range(num_batches)))]
+# results = [load_ppr_topk_parallel(topk_batch_size,
+#                                   input_dir,
+#                                   alpha,
+#                                   eps,
+#                                   topk,
+#                                   ppr_normalization,
+#                                   list(range(num_batches)))]
 
-logging.info("Read took time: " + str(datetime.now() - start))
-results = [item for sublist in results for item in sublist]
+# logging.info("Read took time: " + str(datetime.now() - start))
+# results = [item for sublist in results for item in sublist]
 
-sp_0 = results[0]["partial_ppr"].tocoo()
-edge_rows, edge_cols, edge_vals = sp_0.row, sp_0.col, sp_0.data
-edge_rows += results[0]["batch_id"] * topk_batch_size
+# sp_0 = results[0]["partial_ppr"].tocoo()
+# edge_rows, edge_cols, edge_vals = sp_0.row, sp_0.col, sp_0.data
+# edge_rows += results[0]["batch_id"] * topk_batch_size
 
-row_list = [edge_rows]
-col_list = [edge_cols]
-val_list = [edge_vals]
+# row_list = [edge_rows]
+# col_list = [edge_cols]
+# val_list = [edge_vals]
+# nnz = sp_0.nnz
+# for i in range(1, num_batches):
+#     if results[i]["partial_ppr"] is not None:
+#         spi = results[i]["partial_ppr"].tocoo()
+#         nnz += spi.nnz
+#         edge_rows_i, edge_cols_i, edge_vals_i = spi.row, spi.col, spi.data
+#         edge_rows_i += results[i]["batch_id"] * topk_batch_size
 
-for i in range(1, num_batches):
-    if results[i]["partial_ppr"] is not None:
-        spi = results[i]["partial_ppr"].tocoo()
-        edge_rows_i, edge_cols_i, edge_vals_i = spi.row, spi.col, spi.data
-        edge_rows_i += results[i]["batch_id"] * topk_batch_size
+#         row_list.append(edge_rows_i)
+#         col_list.append(edge_cols_i)
+#         val_list.append(edge_vals_i)
 
-        row_list.append(edge_rows_i)
-        col_list.append(edge_cols_i)
-        val_list.append(edge_vals_i)
+# print(nnz)
+# logging.info(f"Start concatinating")
+# concat_start = datetime.now()
+
+# edge_rows = np.concatenate(row_list)
+# concat_time = datetime.now() - concat_start
+# logging.info(f"Concat took {concat_time} seconds")
+
+# concat_start = datetime.now()
+# edge_cols = np.concatenate(col_list)
+# concat_time = datetime.now() - concat_start
+# logging.info(f"Concat took {concat_time} seconds")
+
+# concat_start = datetime.now()
+# edge_vals = np.concatenate(val_list)
+# concat_time = datetime.now() - concat_start
+# logging.info(f"Concat took {concat_time} seconds")
+
+# create_start = datetime.now()
+# ppr_matrix = sp.coo_matrix((edge_vals, (edge_rows, edge_cols)), shape=(num_nodes, num_nodes))
+# create_time = datetime.now() - create_start
+# logging.info(f"coo creation took {create_time} seconds")
+
+# csr_start = datetime.now()
+# ppr_matrix = ppr_matrix.tocsr()
+# csr_time = datetime.now() - csr_start
+# logging.info(f"to csr took {csr_time} seconds")
+
+# logging.info("Read and convert took time: " + str(datetime.now() - start))
+# print(ppr_matrix.nnz)
 
 
-logging.info(f"Start concatinating")
-concat_start = datetime.now()
-
-edge_rows = np.concatenate(row_list)
-concat_time = datetime.now() - concat_start
-logging.info(f"Concat took {concat_time} seconds")
-
-concat_start = datetime.now()
-edge_cols = np.concatenate(col_list)
-concat_time = datetime.now() - concat_start
-logging.info(f"Concat took {concat_time} seconds")
-
-concat_start = datetime.now()
-edge_vals = np.concatenate(val_list)
-concat_time = datetime.now() - concat_start
-logging.info(f"Concat took {concat_time} seconds")
-
-ppr_matrix = sp.coo_matrix((edge_vals, (edge_rows, edge_cols)), shape=(num_nodes, num_nodes))
-
-logging.info("Read and convert took time: " + str(datetime.now() - start))
-print(ppr_matrix)
+print("done")

pre_calc_ppr.py
@@ -20,7 +20,10 @@ setup_logging()
 
 logging.info("start")
 
-# logging.info("start")
+# whether to calculate the ppr score for all nodes (==True)
+# or just for the training, validation and test nodes (==False)
+calc_ppr_for_all = False
+
 # dataset = "ogbn-arxiv"  # "ogbn-papers100M"  # "ogbn-arxiv"
 # device = "cpu"
 # dataset_root = "/nfs/students/schmidtt/datasets/"
@@ -32,42 +35,63 @@ logging.info("start")
 dataset = "ogbn-papers100M"  # "ogbn-arxiv"  # "ogbn-papers100M"  # "ogbn-arxiv"
 device = 0
 dataset_root = "/nfs/students/schmidtt/datasets/"
-output_dir = dataset_root + "ppr/papers/"
+output_dir = dataset_root + "ppr/papers100M/"
 binary_attr = False
-topk_batch_size = int(1e7)
+normalize = "row"
+make_undirected = True
+make_unweighted = True
+topk_batch_size = int(1e6)
 dir_name = '_'.join(dataset.split('-'))
 
 
 # ppr params
 alpha = 0.1
-eps = 1e-3
-topk = 64
+eps = 1e-8
+topk = 256
 ppr_normalization = "row"
 alpha_suffix = int(alpha * 100)
 
-graph = prep_graph(dataset, "cpu", dataset_root=dataset_root,
-                   make_undirected=False,
-                   make_unweighted=True,
-                   binary_attr=False,
+graph = prep_graph(dataset, "cpu",
+                   dataset_root=dataset_root,
+                   make_undirected=make_undirected,
+                   make_unweighted=make_unweighted,
+                   normalize=normalize,
+                   binary_attr=binary_attr,
                    return_original_split=dataset.startswith('ogbn'))
+
+attr, adj, labels = graph[:3]
+if len(graph) == 3:
+    idx_train, idx_val, idx_test = split(labels.cpu().numpy())
+else:
+    idx_train, idx_val, idx_test = graph[3]['train'], graph[3]['valid'], graph[3]['test']
 
 logging.info("successfully read dataset")
 
 attr, adj, labels = graph[:3]
 num_nodes = attr.shape[0]
+train_num_nodes = len(idx_train)
+val_num_nodes = len(idx_val)
+test_num_nodes = len(idx_test)
 logging.info(f"Dataset has {num_nodes} nodes")
+logging.info(f"Train split has {train_num_nodes} nodes")
+logging.info(f"Val split has {val_num_nodes} nodes")
+logging.info(f"Test split has {test_num_nodes} nodes")
 
 
-def save_ppr_topk(topk_batch_size,
-                  output_dir,
-                  adj_sp,
-                  num_nodes,
-                  alpha,
-                  eps,
-                  topk,
-                  ppr_normalization):
-    dump_suffix = f"{dataset}_alpha{alpha_suffix}_eps{eps:.0e}_topk{topk}_norm{ppr_normalization}"
-    ppr_idx = np.arange(num_nodes)
+def _save_ppr_topk(topk_batch_size,
+                   output_dir,
+                   adj_sp,
+                   ppr_idx,
+                   alpha,
+                   eps,
+                   topk,
+                   ppr_normalization,
+                   make_undirected,
+                   make_unweighted,
+                   normalize,
+                   split_desc):
+    dump_suffix = f"{dataset}_{split_desc}_alpha{alpha_suffix}_eps{eps:.0e}_topk{topk}_pprnorm{ppr_normalization}_norm{normalize}_indirect{make_undirected}_unweighted{make_unweighted}"
+    num_nodes = len(ppr_idx)
     num_batches = math.ceil(num_nodes / topk_batch_size)
     Path(output_dir).mkdir(parents=True, exist_ok=True)
     nnz = 0
@@ -84,7 +108,54 @@ def save_ppr_topk(topk_batch_size,
         file_name = f"topk_ppr_{dump_suffix}_{i:08d}.npz"
         nnz += topk_ppr.nnz
         sp.save_npz(output_dir + file_name, topk_ppr)
+    np.save(output_dir + f"{dump_suffix}_idx.npy", ppr_idx)
     print(nnz)
 
 
-save_ppr_topk(topk_batch_size, output_dir, adj, num_nodes, alpha, eps, topk, ppr_normalization)
+def save_ppr_topk(topk_batch_size,
+                  output_dir,
+                  adj_sp,
+                  alpha,
+                  eps,
+                  topk,
+                  ppr_normalization,
+                  make_undirected,
+                  make_unweighted,
+                  normalize,
+                  calc_ppr_for_all,
+                  idx_train, idx_val, idx_test):
+
+    def save_ppr(ppr_idx, split_desc):
+        _save_ppr_topk(topk_batch_size,
+                       output_dir,
+                       adj,
+                       ppr_idx,
+                       alpha,
+                       eps,
+                       topk,
+                       ppr_normalization,
+                       make_undirected,
+                       make_unweighted,
+                       normalize,
+                       split_desc)
+    if calc_ppr_for_all:
+        save_ppr(np.arange(adj.size(0)), "full")
+    else:
+        save_ppr(idx_train, "train")
+        save_ppr(idx_val, "val")
+        save_ppr(idx_test, "test")
+
+
+save_ppr_topk(topk_batch_size,
+              output_dir,
+              adj,
+              alpha,
+              eps,
+              topk,
+              ppr_normalization,
+              make_undirected,
+              make_unweighted,
+              normalize,
+              calc_ppr_for_all,
+              idx_train, idx_val, idx_test
+              )

rgnn_at_scale/data.py
@@ -664,21 +664,37 @@ def prep_graph(name: str,
             edge_weight = data.edge_attr
         edge_weight = edge_weight.to(device)
 
-        if make_undirected:
-            edge_index, edge_weight = utils.to_symmetric(edge_index, edge_weight, num_nodes)
-
-        if make_unweighted:
-            edge_weight = torch.ones_like(edge_weight)
-
         adj = sp.csr_matrix((edge_weight, edge_index), (num_nodes, num_nodes))
-
-        if normalize:
-            adj = utils.calc_A_hat(adj)
-
-        #adj = torch_sparse.SparseTensor.from_scipy(adj).coalesce()
 
         del edge_index
         del edge_weight
+
+        if make_unweighted:
+            adj.data = np.ones_like(adj.data)
+
+        if make_undirected:
+            logging.info("make undirected")
+            logging.info(ppr_utils.get_max_memory_bytes() / (1024 ** 3))
+
+            adj = utils.to_symmetric_scipy(adj, is_undirected=make_unweighted)
+
+            logging.info(ppr_utils.get_max_memory_bytes() / (1024 ** 3))
+            logging.info("make undirected done")
+
+        if normalize == "row":
+            logging.info("normalize row")
+            logging.info(ppr_utils.get_max_memory_bytes() / (1024 ** 3))
+            adj = utils.normalize_row(adj)
+            logging.info(ppr_utils.get_max_memory_bytes() / (1024 ** 3))
+            logging.info("normalize done")
+        elif normalize:
+            logging.info("normalize symmetric")
+            logging.info(ppr_utils.get_max_memory_bytes() / (1024 ** 3))
+            adj = utils.normalize_symmetric(adj)
+            logging.info(ppr_utils.get_max_memory_bytes() / (1024 ** 3))
+            logging.info("normalize done")
+
+        #adj = torch_sparse.SparseTensor.from_scipy(adj).coalesce()
 
         attr = data.x.to(device)
 

rgnn_at_scale/models.py
@@ -5,6 +5,7 @@ import collections
 from typing import Any, Callable, Dict, Optional, Sequence, Tuple, Union
 
 import time
+from datetime import datetime
 import logging
 import numpy as np
 import scipy.sparse as sp
@@ -686,7 +687,7 @@ class PPRGoWrapperBase():
             )
             num_predictions = topk_ppr.shape[0]
 
-            logits = torch.zeros(num_predictions, self.n_classes, device="cpu", dtype=torch.float32)
+            logits = torch.zeros(num_predictions, self.num_classes, device="cpu", dtype=torch.float32)
 
             num_batches = len(data_loader)
             for batch_id, (idx, xbs, _) in enumerate(data_loader):
@@ -713,9 +714,11 @@ class PPRGoWrapperBase():
             weight_decay: int,
             patience: int,
             use_annealing_scheduler=False,
+            scheduler_warm_restarts=True,
             annealing_scheduler_T_0=3,
             scheduler_time="epoch",
             scheduler_step=20,
+            optim="Adam",
             max_epochs: int = 200,
             batch_size=512,
             batch_mult_val=4,
@@ -738,37 +741,62 @@ class PPRGoWrapperBase():
         else:
             mean = "None"
             temp = 0
-            label = "VanillaPPRGo"
+
+            if hasattr(self, "diffuse_on_embedding"):
+                label = "VanillaPPRGoDiffEmb"
+            else:
+                label = "VanillaPPRGo"
 
         hidden_size = self.hidden_size
         nlayers = self.nlayers
         alpha = self.alpha
         eps = self.eps
         topk = self.topk
+        dropout = self.dropout
+        ppr_normalization = self.ppr_normalization
+        batch_norm = self.mlp.use_batch_norm
 
-        suffix = f"/{label}_alpha{alpha:.0e}_eps{eps:.0e}_topk{topk}_L{nlayers}_H{hidden_size:03d}_T{temp:.0e}_{mean}_LR{lr:.0e}_WD{weight_decay:.0e}_S{use_annealing_scheduler:d}_B{batch_size:03d}"
-        suffix += "_" + str(time.time())
+        suffix = f"/{label}_alpha{alpha:.0e}_eps{eps:.0e}_topk{topk}_L{nlayers}_H{hidden_size:03d}_D{dropout}_BN{batch_norm}"
+        suffix += f"_T{temp:.0e}_{mean}_LR{lr:.0e}_WD{weight_decay:.0e}_S{use_annealing_scheduler:d}_B{batch_size:03d}"
+        suffix += f"_N{ppr_normalization}"
+        suffix += "_" + str(datetime.now().strftime('%d-%m_%H-%M-%S'))
         writer = SummaryWriter(log_dir=log_dir + suffix)
 
         if isinstance(adj, SparseTensor):
             adj = adj.to_scipy(layout="csr")
 
+        logging.info(f"tensorboard: {suffix}")
+        logging.info(ppr_utils.get_max_memory_bytes() / (1024 ** 3))
+
         topk_train = ppr.topk_ppr_matrix(adj, self.alpha, self.eps, idx_train,
                                          self.topk,  normalization=self.ppr_normalization)
 
+        logging.info("topk_train calculated")
+        logging.info(ppr_utils.get_max_memory_bytes() / (1024 ** 3))
+
         topk_val = ppr.topk_ppr_matrix(adj, self.alpha, self.eps, idx_val,
                                        self.topk,  normalization=self.ppr_normalization)
+
+        logging.info("topk_val calculated")
+        logging.info(ppr_utils.get_max_memory_bytes() / (1024 ** 3))
 
         train_set = RobustPPRDataset(attr_matrix_all=attr,
                                      ppr_matrix=topk_train,
                                      indices=idx_train,
                                      labels_all=labels,
                                      allow_cache=False)
+
+        logging.info("train_set initalized")
+        logging.info(ppr_utils.get_max_memory_bytes() / (1024 ** 3))
+
         val_set = RobustPPRDataset(attr_matrix_all=attr,
                                    ppr_matrix=topk_val,
                                    indices=idx_val,
                                    labels_all=labels,
                                    allow_cache=False)
+
+        logging.info("val_set initalized")
+        logging.info(ppr_utils.get_max_memory_bytes() / (1024 ** 3))
 
         train_loader = torch.utils.data.DataLoader(
             dataset=train_set,
@@ -779,6 +807,10 @@ class PPRGoWrapperBase():
             batch_size=None,
             num_workers=0,
         )
+
+        logging.info("train_loader initalized")
+        logging.info(ppr_utils.get_max_memory_bytes() / (1024 ** 3))
+
         trace_train_loss = []
         trace_val_loss = []
         trace_train_acc = []
@@ -788,11 +820,18 @@ class PPRGoWrapperBase():
             for param_group in optimizer.param_groups:
                 return param_group['lr']
 
-        optimizer = torch.optim.Adam(self.parameters(), lr=lr, weight_decay=weight_decay)
+        if optim == "SGD":
+            optimizer = torch.optim.SGD(self.parameters(), lr=lr, weight_decay=weight_decay)
+        else:  # use adam
+            optimizer = torch.optim.Adam(self.parameters(), lr=lr, weight_decay=weight_decay)
 
         if use_annealing_scheduler:
-            scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
-                optimizer, annealing_scheduler_T_0)
+            if scheduler_warm_restarts:
+                scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
+                    optimizer, annealing_scheduler_T_0)
+            else:
+                scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+                    optimizer, max_epochs)
 
         best_loss = np.inf
 
@@ -859,7 +898,7 @@ class PPRGoWrapperBase():
 
             # restore the best validation state
         self.load_state_dict(best_state)
-        return {"loss": trace_train_loss, "acc": trace_train_acc}, {"loss": trace_val_loss, "acc": trace_val_acc},
+        return {"loss": trace_val_loss, "acc": trace_val_acc}, {"loss": trace_train_loss, "acc": trace_train_acc}
 
     def __run_batch(self, xbs, yb, optimizer, train):
         # Set model to training mode
@@ -901,9 +940,11 @@ class PPRGoWrapper(PPRGo, PPRGoWrapperBase):
                  forward_batch_size=128,
                  **kwargs):
         super().__init__(n_features, n_classes, hidden_size, nlayers, dropout, **kwargs)
-        self.nlayers = nlayers
+        self.num_features = n_features
+        self.num_classes = n_classes
         self.hidden_size = hidden_size
-        self.n_classes = n_classes
+        self.nlayers = nlayers
+        self.dropout = dropout
         self.alpha = alpha
         self.eps = eps
         self.topk = topk
@@ -936,9 +977,12 @@ class PPRGoDiffEmbWrapper(PPRGoEmmbeddingDiffusions, PPRGoWrapperBase):
                  forward_batch_size=128,
                  **kwargs):
         super().__init__(n_features, n_classes, hidden_size, nlayers, dropout, **kwargs)
-        self.nlayers = nlayers
+        self.diffuse_on_embedding = True
+        self.num_features = n_features
+        self.num_classes = n_classes
         self.hidden_size = hidden_size
-        self.n_classes = n_classes
+        self.nlayers = nlayers
+        self.dropout = dropout
         self.alpha = alpha
         self.eps = eps
         self.topk = topk
@@ -973,9 +1017,11 @@ class RobustPPRGoWrapper(RobustPPRGo, PPRGoWrapperBase):
                  **kwargs):
         super().__init__(n_features, n_classes, hidden_size, nlayers, dropout, mean=mean,  **kwargs)
         self.mean = mean
-        self.nlayers = nlayers
+        self.num_features = n_features
+        self.num_classes = n_classes
         self.hidden_size = hidden_size
-        self.n_classes = n_classes
+        self.nlayers = nlayers
+        self.dropout = dropout
         self.alpha = alpha
         self.eps = eps
         self.topk = topk
@@ -1006,9 +1052,11 @@ class RobustPPRGoDiffEmbWrapper(RobustPPRGoEmmbeddingDiffusions, PPRGoWrapperBas
         super().__init__(n_features, n_classes, hidden_size, nlayers, dropout, mean=mean,  **kwargs)
         self.diffuse_on_embedding = True
         self.mean = mean
-        self.nlayers = nlayers
+        self.num_features = n_features
+        self.num_classes = n_classes
         self.hidden_size = hidden_size
-        self.n_classes = n_classes
+        self.nlayers = nlayers
+        self.dropout = dropout
         self.alpha = alpha
         self.eps = eps
         self.topk = topk

rgnn_at_scale/utils.py
@@ -1,16 +1,19 @@
 """For the util methods such as conversions or adjacency preprocessings.
 """
 from typing import Sequence, Tuple, Union
+import logging
 
 import numba
 import numpy as np
 import scipy.sparse as sp
+from sklearn.preprocessing import normalize
 import torch
 
 from torch_geometric.utils import from_scipy_sparse_matrix, add_remaining_self_loops
 import torch_scatter
 import torch_sparse
 from torch_sparse import SparseTensor, transpose, spmm, coalesce
+from pprgo import utils as ppr_utils
 
 
 # TODO: Move to base attack
@@ -52,9 +55,13 @@ def calc_A_hat(adj_matrix: sp.spmatrix) -> sp.spmatrix:
     """
     nnodes = adj_matrix.shape[0]
     A = adj_matrix + sp.eye(nnodes)
-    D_vec = np.sum(A, axis=1).A1
-    D_vec_invsqrt_corr = 1 / np.sqrt(D_vec)
+    logging.info(ppr_utils.get_max_memory_bytes() / (1024 ** 3))
+
+    D_vec_invsqrt_corr = 1 / np.sqrt(np.sum(A, axis=1).A1)
+    logging.info(ppr_utils.get_max_memory_bytes() / (1024 ** 3))
+
     D_invsqrt_corr = sp.diags(D_vec_invsqrt_corr)
+    logging.info(ppr_utils.get_max_memory_bytes() / (1024 ** 3))
     return D_invsqrt_corr @ A @ D_invsqrt_corr
 
 
@@ -614,7 +621,11 @@ def to_symmetric(edge_index: torch.Tensor, edge_weight: torch.Tensor,
     symmetric_edge_index = torch.cat(
         (edge_index, edge_index.flip(0)), dim=-1
     )
+    logging.info(ppr_utils.get_max_memory_bytes() / (1024 ** 3))
+
     symmetric_edge_weight = edge_weight.repeat(2)
+    logging.info(ppr_utils.get_max_memory_bytes() / (1024 ** 3))
+
     symmetric_edge_index, symmetric_edge_weight = torch_sparse.coalesce(
         symmetric_edge_index,
         symmetric_edge_weight,
@@ -623,6 +634,34 @@ def to_symmetric(edge_index: torch.Tensor, edge_weight: torch.Tensor,
         op=op
     )
     return symmetric_edge_index, symmetric_edge_weight
+
+
+def to_symmetric_scipy(adjacency: sp.csr_matrix, is_undirected: bool):
+    logging.info(ppr_utils.get_max_memory_bytes() / (1024 ** 3))
+    if is_undirected:
+        sym_adjacency = (adjacency + adjacency.T).astype(bool)
+    else:
+        sym_adjacency = adjacency.astype(float)
+        sym_adjacency += adjacency.T
+
+    logging.info(ppr_utils.get_max_memory_bytes() / (1024 ** 3))
+    sym_adjacency.tocsr().sort_indices()
+
+    return sym_adjacency
+
+
+def normalize_row(adj_matrix: sp.spmatrix) -> sp.spmatrix:
+    return normalize(adj_matrix, norm='l1', axis=1)
+
+
+def normalize_symmetric(adj_matrix: sp.spmatrix) -> sp.spmatrix:
+
+    D_vec_invsqrt_corr = 1 / np.sqrt(np.sum(adj_matrix, axis=1).A1)
+    logging.info(ppr_utils.get_max_memory_bytes() / (1024 ** 3))
+
+    D_invsqrt_corr = sp.diags(D_vec_invsqrt_corr)
+    logging.info(ppr_utils.get_max_memory_bytes() / (1024 ** 3))
+    return D_invsqrt_corr @ adj_matrix @ D_invsqrt_corr
 
 
 def sparse_tensor(spmat: sp.spmatrix, grad: bool = False):