6d033ee99002dde8aab67c610bc61ce2ca8795a0
Tobias Schmidt <tob.schmidt@tum.de>
2021-04-10 21:56:50 +0200

    fix: more efficient ppr precalc

--- rgnn_at_scale/helper/ppr_load.py
+++ rgnn_at_scale/helper/ppr_load.py
@@ -4,19 +4,67 @@ import os
 import glob
 from pathlib import Path
 
+import array
 import numpy as np
 import scipy.sparse as sp
 from datetime import datetime
+from rgnn_at_scale.helper import utils
+
+
+class IncrementalCSRMatrix(object):
+
+    def __init__(self, shape, dtype):
+
+        if dtype is np.dtype(np.int32):
+            type_flag = 'i'
+        elif dtype is np.dtype(np.int64):
+            type_flag = 'l'
+        elif dtype is np.dtype(np.float32):
+            type_flag = 'f'
+        elif dtype is np.dtype(np.float64):
+            type_flag = 'd'
+        else:
+            raise Exception('Dtype not supported.')
+
+        self.dtype = dtype
+        self.shape = shape
+
+        self.rows = array.array('i')
+        self.cols = array.array('i')
+        self.data = array.array(type_flag)
+
+    def append(self, row, col, val):
+
+        m, n = self.shape
+
+        for r, c, v in zip(row, col, val):
+            self.rows.append(r)
+            self.cols.append(c)
+            self.data.append(v)
+
+    def tocsr(self):
+
+        rows = np.frombuffer(self.rows, dtype=np.int32)
+        logging.info("load rows")
+        cols = np.frombuffer(self.cols, dtype=np.int32)
+        logging.info("load cols")
+        data = np.frombuffer(self.data, dtype=self.dtype)
+        logging.info("load vals")
+
+        return sp.csr_matrix((data, (rows, cols)),
+                             shape=self.shape)
+
+    def __len__(self):
+
+        return len(self.data)
 
 
 def _load_ppr(input_dir, dump_suffix, shape):
     batch_id = 0
     last_row = 0
 
-    row_list = []
-    col_list = []
-    val_list = []
-
+    incremental_adj = None
+    start_read = datetime.now()
     while True:
         start_batch = datetime.now()
         logging.info(f"Read batch {batch_id}")
@@ -31,44 +79,60 @@ def _load_ppr(input_dir, dump_suffix, shape):
         edge_rows_i, edge_cols_i, edge_vals_i = spi.row, spi.col, spi.data
         edge_rows_i += last_row
 
-        row_list.append(edge_rows_i)
-        col_list.append(edge_cols_i)
-        val_list.append(edge_vals_i)
+        if incremental_adj is None:
+            incremental_adj = IncrementalCSRMatrix(shape, edge_vals_i.dtype)
+
+        incremental_adj.append(edge_rows_i, edge_cols_i, edge_vals_i)
 
         batch_id += 1
         last_row = edge_rows_i.max() + 1
 
         read_time = datetime.now() - start_batch
         logging.info(f"Read {batch_id} took {read_time} seconds")
+        logging.info(f'Memory when reading ppr: {utils.get_max_memory_bytes() / (1024 ** 3)}')
 
-    logging.info("Start concatinating")
+    read_time = datetime.now() - start_read
+    logging.info(f"Read took {read_time} seconds")
+    logging.info(f'Memory after reading ppr: {utils.get_max_memory_bytes() / (1024 ** 3)}')
+
+    # logging.info("Start concatinating")
+    # concat_start = datetime.now()
+    # edge_rows = np.concatenate(row_list)
+    # concat_time = datetime.now() - concat_start
+    # del row_list
+    # logging.info(f"Concat took {concat_time} seconds")
+    # logging.info(f'Memory concat row ppr: {utils.get_max_memory_bytes() / (1024 ** 3)}')
+
+    # concat_start = datetime.now()
+    # edge_cols = np.concatenate(col_list)
+    # concat_time = datetime.now() - concat_start
+    # del col_list
+    # logging.info(f"Concat took {concat_time} seconds")
+    # logging.info(f'Memory concat col ppr: {utils.get_max_memory_bytes() / (1024 ** 3)}')
+
+    # concat_start = datetime.now()
+    # edge_vals = np.concatenate(val_list)
+    # concat_time = datetime.now() - concat_start
+    # del val_list
+    # logging.info(f"Concat took {concat_time} seconds")
+    # logging.info(f'Memory concat val ppr: {utils.get_max_memory_bytes() / (1024 ** 3)}')
+
+    # if shape is None:
+    #     # guess shape
+    #     max_row = edge_rows.max() + 1
+    #     max_col = edge_cols.max() + 1
+    #     # guess shape is square
+    #     max_nodes = max(max_row, max_col)
+    #     shape = (max_nodes, max_nodes)
+
     concat_start = datetime.now()
-    edge_rows = np.concatenate(row_list)
-    concat_time = datetime.now() - concat_start
-    logging.info(f"Concat took {concat_time} seconds")
+    logging.info("try build adj")
+    adj = incremental_adj.tocsr()
 
-    concat_start = datetime.now()
-    edge_cols = np.concatenate(col_list)
-    concat_time = datetime.now() - concat_start
-    logging.info(f"Concat took {concat_time} seconds")
-
-    concat_start = datetime.now()
-    edge_vals = np.concatenate(val_list)
-    concat_time = datetime.now() - concat_start
-    logging.info(f"Concat took {concat_time} seconds")
-
-    concat_start = datetime.now()
-    if shape is None:
-        # guess shape
-        max_row = edge_rows.max() + 1
-        max_col = edge_cols.max() + 1
-        # guess shape is square
-        max_nodes = max(max_row, max_col)
-        shape = (max_nodes, max_nodes)
-
-    adj = sp.csr_matrix((edge_vals, (edge_rows, edge_cols)), shape=shape)
     concat_time = datetime.now() - concat_start
     logging.info(f"Building csr took {concat_time} seconds")
+    logging.info(f'Memory after building ppr: {utils.get_max_memory_bytes() / (1024 ** 3)}')
+
     return adj
 
 
