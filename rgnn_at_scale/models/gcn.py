fix: normalization on cpu with approx ppr

rgnn_at_scale/models/gcn.py
@@ -259,10 +259,12 @@ class GCN(nn.Module):
                                      edge_weight: Optional[torch.Tensor] = None
                                      ) -> Tuple[Union[torch.Tensor, SparseTensor], Optional[torch.Tensor]]:
         if self.gdc_params is not None:
-            edge_idx, edge_weight = GCN.normalize(edge_idx, x.shape[0], edge_weight, self.add_self_loops)
+            n = x.shape[0]
             if 'use_cpu' in self.gdc_params and self.gdc_params['use_cpu']:
-                edge_idx, edge_weight = get_approx_topk_ppr_matrix(edge_idx, x.shape[0], **self.gdc_params)
+                edge_idx, _ = add_remaining_self_loops(edge_idx.cpu(), None, 1., n)
+                edge_idx, edge_weight = get_approx_topk_ppr_matrix(edge_idx, n, **self.gdc_params)
             else:
+                edge_idx, edge_weight = GCN.normalize(edge_idx, n, edge_weight, self.add_self_loops)
                 adj = get_ppr_matrix(torch.sparse.FloatTensor(edge_idx, edge_weight), **self.gdc_params)
                 edge_idx, edge_weight = adj.indices(), adj.values()
                 del adj